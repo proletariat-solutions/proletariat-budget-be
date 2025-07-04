6868c989e3fa84371fd289815442c0d709cee6fd
ivan puszkiel <havanaguilahava@gmail.com>
2025-07-04 14:57:25 -0300

    feat: saving contributions and withdrawals. Now both operations operate over a transfer instead of a transaction directly.

--- internal/adapter/driven/mysql/saving_goal.go
+++ internal/adapter/driven/mysql/saving_goal.go
@@ -12,9 +12,8 @@ import (
 )
 
 type SavingGoalRepoImpl struct {
-	db           *sql.DB
-	categoryRepo *port.CategoryRepo
-	tagsRepo     *port.TagsRepo
+	db       *sql.DB
+	tagsRepo *port.TagsRepo
 }
 
 func (s SavingGoalRepoImpl) Create(ctx context.Context, savingsGoal openapi.SavingsGoalRequest) (string, error) {
@@ -124,13 +123,46 @@ func (s SavingGoalRepoImpl) Delete(ctx context.Context, id string) error {
 }
 
 func (s SavingGoalRepoImpl) GetByID(ctx context.Context, id string) (*openapi.SavingsGoal, error) {
-	query := `SELECT id, name, category_id, description, target_amount, currency, target_date, initial_amount, current_amount, percent_complete, account_id, priority, auto_contribute, auto_contribute_amount, auto_contribute_frequency, status, created_at, updated_at FROM savings_goals WHERE id=? AND status='active'`
+	query := `select sg.id,
+					   sg.name,
+					   sg.description,
+					   sg.target_amount,
+					   sg.currency,
+					   sg.target_date,
+					   sg.initial_amount,
+					   sg.current_amount,
+					   sg.percent_complete,
+					   sg.account_id,
+					   sg.priority,
+					   sg.auto_contribute,
+					   sg.auto_contribute_amount,
+					   sg.auto_contribute_frequency,
+					   sg.status,
+					   sg.projected_completion_date,
+					   sg.created_at,
+					   sg.updated_at,
+					   GROUP_CONCAT(sgt.tag_id ORDER BY sgt.tag_id SEPARATOR ',') as tags,
+					   c.id,
+					   c.name,
+					   c.description,
+					   c.color,
+					   c.background_color,
+					   c.active,
+					   c.category_type
+				from savings_goals sg
+						 inner join categories c ON sg.category_id = c.id
+						 left join savings_goal_tags sgt ON sg.id = sgt.savings_goal_id
+				where sg.id =?
+				group by sg.id, sg.name, sg.description, sg.target_amount, sg.currency, sg.target_date, sg.initial_amount,
+						 sg.current_amount, sg.percent_complete, sg.account_id, sg.priority, sg.auto_contribute,
+						 sg.auto_contribute_amount, sg.auto_contribute_frequency, sg.status, sg.projected_completion_date,
+						 sg.created_at, sg.updated_at
+`
 	var savingsGoal openapi.SavingsGoal
-	var categoryID string
+	var tags string
 	err := s.db.QueryRowContext(ctx, query, id).Scan(
 		&savingsGoal.Id,
 		&savingsGoal.Name,
-		&categoryID,
 		&savingsGoal.Description,
 		&savingsGoal.TargetAmount,
 		&savingsGoal.Currency,
@@ -144,8 +176,16 @@ func (s SavingGoalRepoImpl) GetByID(ctx context.Context, id string) (*openapi.Sa
 		&savingsGoal.AutoContributeAmount,
 		&savingsGoal.AutoContributeFrequency,
 		&savingsGoal.Status,
+		&savingsGoal.ProjectedCompletionDate,
 		&savingsGoal.CreatedAt,
 		&savingsGoal.UpdatedAt,
+		&tags,
+		&savingsGoal.Category.Id,
+		&savingsGoal.Category.Name,
+		&savingsGoal.Category.Description,
+		&savingsGoal.Category.Color,
+		&savingsGoal.Category.BackgroundColor,
+		&savingsGoal.Category.Active,
 	)
 	if errors.Is(err, sql.ErrNoRows) {
 		return nil, fmt.Errorf("savings goal not found")
@@ -153,38 +193,50 @@ func (s SavingGoalRepoImpl) GetByID(ctx context.Context, id string) (*openapi.Sa
 		return nil, fmt.Errorf("failed to select savings goal: %w", err)
 	}
 
-	category, err := (*s.categoryRepo).GetByID(ctx, categoryID)
+	savingsGoal.Tags, err = (*s.tagsRepo).GetByIDs(ctx, strings.Split(tags, ","))
 	if err != nil {
-		return nil, fmt.Errorf("failed to get category: %w", err)
+		return nil, fmt.Errorf("failed to get tags: %w", err)
 	}
-	savingsGoal.Category = *category
+
 	return &savingsGoal, nil
 }
 
 func (s SavingGoalRepoImpl) List(ctx context.Context) ([]openapi.SavingsGoal, error) {
-	query := `SELECT 
-				id, 
-				name, 
-				category_id, 
-				description, 
-				target_amount, 
-				currency, 
-				target_date, 
-				initial_amount, 
-				current_amount, 
-				percent_complete, 
-				account_id, 
-				priority, 
-				auto_contribute, 
-				auto_contribute_amount, 
-				auto_contribute_frequency, 
-				status, 
-				created_at, 
-				updated_at,
-			    GROUP_CONCAT(sgt.tag_id ORDER BY sgt.tag_id SEPARATOR ',') as tags
-			FROM savings_goals
-			left join savings_goal_tags sgt ON savings_goals.id = sgt.savings_goal_id
-			WHERE status != 'inactive'`
+	query := `select sg.id,
+					   sg.name,
+					   sg.description,
+					   sg.target_amount,
+					   sg.currency,
+					   sg.target_date,
+					   sg.initial_amount,
+					   sg.current_amount,
+					   sg.percent_complete,
+					   sg.account_id,
+					   sg.priority,
+					   sg.auto_contribute,
+					   sg.auto_contribute_amount,
+					   sg.auto_contribute_frequency,
+					   sg.status,
+					   sg.projected_completion_date,
+					   sg.created_at,
+					   sg.updated_at,
+					   GROUP_CONCAT(sgt.tag_id ORDER BY sgt.tag_id SEPARATOR ',') as tags,
+					   c.id,
+					   c.name,
+					   c.description,
+					   c.color,
+					   c.background_color,
+					   c.active,
+					   c.category_type
+				from savings_goals sg
+						 inner join categories c ON sg.category_id = c.id
+						 left join savings_goal_tags sgt ON sg.id = sgt.savings_goal_id
+				where sg.status != 'inactive'
+				group by sg.id, sg.name, sg.description, sg.target_amount, sg.currency, sg.target_date, sg.initial_amount,
+						 sg.current_amount, sg.percent_complete, sg.account_id, sg.priority, sg.auto_contribute,
+						 sg.auto_contribute_amount, sg.auto_contribute_frequency, sg.status, sg.projected_completion_date,
+						 sg.created_at, sg.updated_at
+`
 	var savingsGoals []openapi.SavingsGoal
 	var ids []string
 	tagsByID := make(map[string][]string)
@@ -195,12 +247,10 @@ func (s SavingGoalRepoImpl) List(ctx context.Context) ([]openapi.SavingsGoal, er
 	defer rows.Close()
 	for rows.Next() {
 		var savingsGoal openapi.SavingsGoal
-		var categoryID string
 		var tags string
 		err := rows.Scan(
 			&savingsGoal.Id,
 			&savingsGoal.Name,
-			&categoryID,
 			&savingsGoal.Description,
 			&savingsGoal.TargetAmount,
 			&savingsGoal.Currency,
@@ -214,9 +264,16 @@ func (s SavingGoalRepoImpl) List(ctx context.Context) ([]openapi.SavingsGoal, er
 			&savingsGoal.AutoContributeAmount,
 			&savingsGoal.AutoContributeFrequency,
 			&savingsGoal.Status,
+			&savingsGoal.ProjectedCompletionDate,
 			&savingsGoal.CreatedAt,
 			&savingsGoal.UpdatedAt,
 			&tags,
+			&savingsGoal.Category.Id,
+			&savingsGoal.Category.Name,
+			&savingsGoal.Category.Description,
+			&savingsGoal.Category.Color,
+			&savingsGoal.Category.BackgroundColor,
+			&savingsGoal.Category.Active,
 		)
 
 		tagsByID[*savingsGoal.Id] = strings.Split(tags, ",")
@@ -225,11 +282,6 @@ func (s SavingGoalRepoImpl) List(ctx context.Context) ([]openapi.SavingsGoal, er
 			return nil, fmt.Errorf("failed to scan row: %w", err)
 		}
 		ids = append(ids, *savingsGoal.Id)
-		category, err := (*s.categoryRepo).GetByID(ctx, categoryID)
-		if err != nil {
-			return nil, fmt.Errorf("failed to get category: %w", err)
-		}
-		savingsGoal.Category = *category
 	}
 
 	tags, err := (*s.tagsRepo).ListByType(ctx, "savings_goal", &ids)
@@ -290,34 +342,159 @@ func (s SavingGoalRepoImpl) MarkAsAbandoned(ctx context.Context, id string) erro
 	return nil
 }
 
-func (s SavingGoalRepoImpl) CreateWithdrawal(ctx context.Context, withdrawal openapi.SavingsWithdrawalRequest) (string, error) {
-	//TODO implement me
-	panic("implement me")
+func (s SavingGoalRepoImpl) CreateWithdrawal(ctx context.Context, withdrawal openapi.SavingsWithdrawalRequest, transferID, goalID string) (string, error) {
+	queryInsert := `insert into savings_withdrawals 
+						(savings_goal_id, date, reason, transfer_id) 
+					VALUES (?,?,?,?)`
+	result, errInsert := s.db.ExecContext(
+		ctx,
+		queryInsert,
+		goalID,
+		withdrawal.Date,
+		withdrawal.Reason,
+		transferID,
+	)
+	if errInsert != nil {
+		return "", fmt.Errorf("failed to insert savings withdrawal: %w", errInsert)
+	}
+	withdrawalID, err := result.LastInsertId()
+	if err != nil {
+		return "", fmt.Errorf("failed to get last insert ID: %w", err)
+	}
+
+	return fmt.Sprintf("%d", withdrawalID), nil
 }
 
 func (s SavingGoalRepoImpl) DeleteWithdrawal(ctx context.Context, id string) error {
-	//TODO implement me
-	panic("implement me")
+	queryDelete := `DELETE FROM savings_withdrawals WHERE id=?`
+	_, errDelete := s.db.ExecContext(
+		ctx,
+		queryDelete,
+		id,
+	)
+	if errDelete != nil {
+		return fmt.Errorf("failed to delete savings withdrawal: %w", errDelete)
+	}
+	return nil
 }
 
 func (s SavingGoalRepoImpl) GetWithdrawalByID(ctx context.Context, id string) (*openapi.SavingsWithdrawal, error) {
-	//TODO implement me
-	panic("implement me")
+	query := `select sw.id,
+					   sw.savings_goal_id,
+					   sw.date,
+					   sw.reason,
+					   sw.created_at,
+					   sw.updated_at,
+					   tr.destination_amount,
+					   tr.destination_account_id,
+					   t.description,
+					   GROUP_CONCAT(swt.tag_id ORDER BY swt.tag_id SEPARATOR ',') as tags
+				from savings_withdrawals sw
+						 inner join transfers tr ON sw.transfer_id = tr.id
+						 inner join transactions t ON tr.transaction_id = t.id
+						 left join savings_withdrawal_tags swt ON sw.id = swt.withdrawal_id
+				group by sw.id, sw.savings_goal_id, sw.date, sw.reason, sw.transfer_id, sw.created_at, sw.updated_at,
+						 tr.destination_amount, tr.destination_account_id, t.description`
+	row := s.db.QueryRowContext(ctx, query, id)
+	var withdrawal openapi.SavingsWithdrawal
+	var tags string
+	err := row.Scan(
+		&withdrawal.Id,
+		&withdrawal.SavingsGoalId,
+		&withdrawal.Date,
+		&withdrawal.Reason,
+		&withdrawal.CreatedAt,
+		&withdrawal.UpdatedAt,
+		&withdrawal.Amount,
+		&withdrawal.DestinationAccountId,
+		&withdrawal.Notes,
+		&tags,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to scan row: %w", err)
+	}
+
+	withdrawal.Tags, err = (*s.tagsRepo).GetByIDs(ctx, strings.Split(tags, ","))
+	if err != nil {
+		return nil, fmt.Errorf("failed to get tags: %w", err)
+	}
+	return &withdrawal, nil
 }
 
-func (s SavingGoalRepoImpl) CreateContribution(ctx context.Context, contribution openapi.SavingsContributionRequest) (string, error) {
-	//TODO implement me
-	panic("implement me")
+func (s SavingGoalRepoImpl) CreateContribution(ctx context.Context, contribution openapi.SavingsContributionRequest, transferID, goalID string) (string, error) {
+	queryInsert := `insert into savings_contributions (savings_goal_id, date, transfer_id) VALUES (?,?,?)`
+	result, errInsert := s.db.ExecContext(
+		ctx,
+		queryInsert,
+		goalID,
+		contribution.Date,
+		transferID,
+	)
+	if errInsert != nil {
+		return "", fmt.Errorf("failed to insert savings contribution: %w", errInsert)
+	}
+	contributionID, err := result.LastInsertId()
+	if err != nil {
+		return "", fmt.Errorf("failed to get last insert ID: %w", err)
+	}
+	return fmt.Sprintf("%d", contributionID), nil
 }
 
 func (s SavingGoalRepoImpl) DeleteContribution(ctx context.Context, id string) error {
-	//TODO implement me
-	panic("implement me")
+	queryDelete := `DELETE FROM savings_contributions WHERE id=?`
+	_, errDelete := s.db.ExecContext(
+		ctx,
+		queryDelete,
+		id,
+	)
+	if errDelete != nil {
+		return fmt.Errorf("failed to delete savings contribution: %w", errDelete)
+	}
+	return nil
 }
 
 func (s SavingGoalRepoImpl) GetContributionByID(ctx context.Context, id string) (*openapi.SavingsContribution, error) {
-	//TODO implement me
-	panic("implement me")
+	query := `select sc.id,
+					   sc.savings_goal_id,
+					   sc.date,
+					   sc.created_at,
+					   sc.updated_at,
+					   tr.destination_amount,
+					   tr.source_account_id,
+					   GROUP_CONCAT(sct.tag_id ORDER BY sct.tag_id SEPARATOR ',') as tags
+				from savings_contributions sc
+						 inner join transfers tr on sc.transfer_id = tr.id
+						 inner join transactions t on tr.transaction_id = t.id
+						 left join savings_contribution_tags sct on sc.id = sct.contribution_id
+				where sc.id = ?
+				group by sc.id,
+						 sc.savings_goal_id,
+						 sc.date,
+						 sc.created_at,
+						 sc.updated_at,
+						 tr.destination_amount,
+						 tr.source_account_id`
+	row := s.db.QueryRowContext(ctx, query, id)
+	var contribution openapi.SavingsContribution
+	var tags string
+	err := row.Scan(
+		&contribution.Id,
+		&contribution.SavingsGoalId,
+		&contribution.Date,
+		&contribution.CreatedAt,
+		&contribution.UpdatedAt,
+		&contribution.Amount,
+		&contribution.SourceAccountId,
+		&tags,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to scan row: %w", err)
+	}
+	contribution.Tags, err = (*s.tagsRepo).GetByIDs(ctx, strings.Split(tags, ","))
+	if err != nil {
+		return nil, fmt.Errorf("failed to get tags: %w", err)
+	}
+	return &contribution, nil
 }
 
 func (s SavingGoalRepoImpl) ListSavingsTransactions(ctx context.Context, params openapi.ListSavingsTransactionsParams) (*openapi.SavingsTransactionList, error) {
--- internal/core/port/saving_goal.go
+++ internal/core/port/saving_goal.go
@@ -16,12 +16,12 @@ type SavingsGoalRepo interface {
 	MarkAsAbandoned(ctx context.Context, id string) error
 
 	// Withdrawal operations
-	CreateWithdrawal(ctx context.Context, withdrawal openapi.SavingsWithdrawalRequest) (string, error)
+	CreateWithdrawal(ctx context.Context, withdrawal openapi.SavingsWithdrawalRequest, transferID, goalID string) (string, error)
 	DeleteWithdrawal(ctx context.Context, id string) error
 	GetWithdrawalByID(ctx context.Context, id string) (*openapi.SavingsWithdrawal, error)
 
 	// Contribution operations
-	CreateContribution(ctx context.Context, contribution openapi.SavingsContributionRequest) (string, error)
+	CreateContribution(ctx context.Context, contribution openapi.SavingsContributionRequest, transferID, goalID string) (string, error)
 	DeleteContribution(ctx context.Context, id string) error
 	GetContributionByID(ctx context.Context, id string) (*openapi.SavingsContribution, error)
 
